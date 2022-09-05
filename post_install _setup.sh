bcdcc87d06c07d23c10ea81eec6ed5c1fe3d771d
Charlie Taylor <ct9918@outlook.com>
2022-09-05 14:07:29 +0100

    Amended wording

--- post_install _setup.sh
+++ post_install _setup.sh
@@ -4,7 +4,7 @@
 
 #Check if script is run as root
 if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  echo "You must be a root user to run this script." 2>&1
   exit 1
 fi
 
